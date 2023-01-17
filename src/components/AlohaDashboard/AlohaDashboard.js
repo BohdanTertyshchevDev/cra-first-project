64554d71f98959737068535fe6b38f9df69b4462
Bohdan Tertyshchev <bobterya@gmail.com>
2023-01-17 08:08:18 +0200

    Sort user

--- src/components/AlohaDashboard/AlohaDashboard.js
+++ src/components/AlohaDashboard/AlohaDashboard.js
@@ -2,29 +2,51 @@ import React from "react";
 import Aloha from "../Aloha/Aloha"; 
 
 
-const userArray = [
-    {
-        firstName: "Tony",
-        lastName: "Stark"
-    },
-    {
-        firstName: "Druc",
-        lastName: "Bener"
-    },
-    {
-        firstName: "Peter",
-        lastName: "Parker"
-    }
-]
-
-
 class AlohaDashboard extends React.Component{
+    constructor(props) {
+        super(props);
+        this.state = {
+            userArray :[
+                {
+                    firstName: "Tony",
+                    lastName: "Stark"
+                },
+                {
+                    firstName: "Druc",
+                    lastName: "Bener"
+                },
+                {
+                    firstName: "Peter",
+                    lastName: "Parker"
+                }
+            ]
+        }
+    }
+
     userToAloha() {
+        const {userArray} =  this.state;
         return userArray.map((user) => <Aloha name={user.firstName} lastName={user.lastName}/>)
     }
+
+    sortUser() {
+        const {userArray} =  this.state;
+        const newArray = [...userArray];
+        newArray.sort((a, b) =>{
+            if(a.firstName > b.firstName) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+        this.setState({
+            userArray: newArray
+        })
+    }
+
     render() {
         return(
             <>
+                <button onClick={() => {this.sortUser()}}>Sort</button>
                 {this.userToAloha()}
             </>
         )
