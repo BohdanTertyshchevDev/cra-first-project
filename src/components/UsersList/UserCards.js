e8bde928616f733bbf3ae716666f745446eb7828
Bohdan Tertyshchev <bobterya@gmail.com>
2023-01-27 12:26:58 +0200

    PropTypes

--- src/components/UsersList/UserCards.js
+++ src/components/UsersList/UserCards.js
@@ -1,15 +1,33 @@
+import PropTypes from 'prop-types';
+import React from 'react';
 import './style.css';
 
-function UserCards(props) {
-    const {users: {name: {first: firstName, last: lastName}, email, picture: {large: imgSrc}}} = props;
 
-    return (
-        <div className='card-wrapper'>
-            <img src={imgSrc} alt={`${firstName} ${lastName}`} className="user-img"></img>
-            <h1>{firstName} {lastName}</h1>
-            <p>{email}</p>
-        </div>
-    )
+class UserCards extends React.Component {
+    render() {
+        const {users: {name: {first: firstName, last: lastName}, email, picture: {large: imgSrc}}} = this.props;
+
+        return (
+            <div className='card-wrapper'>
+                <img src={imgSrc} alt={`${firstName} ${lastName}`} className="user-img"></img>
+                <h1>{firstName} {lastName}</h1>
+                <p>{email}</p>
+            </div>
+        )
+    }
+}
+
+UserCards.propTypes = {
+    user: PropTypes.shape({
+        name: PropTypes.shape({
+            first: PropTypes.string,
+            last: PropTypes.string
+        }),
+        email: PropTypes.string.isRequired,
+        picture: PropTypes.shape({
+            large: PropTypes.string.isRequired
+        })
+    }).isRequired
 }
 
 export default UserCards;
