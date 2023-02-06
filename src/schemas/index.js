87e96d2401f7a0057fc13f220ae979fa6b4d17f1
Bohdan Tertyshchev <bobterya@gmail.com>
2023-02-06 15:32:58 +0200

    Formik validation

--- src/schemas/index.js
+++ src/schemas/index.js
@@ -5,5 +5,5 @@ export const SCHEMA = yup.object({
     firstName: yup.string().min(1).max(30),
     lastName: yup.string().min(1).max(30),
     email: yup.string().required().email(),
-    pass: yup.string().required().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/)
+    pass: yup.string().required().matches(/^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$ %^&*-]).{8,}$/,"Is not valid")
 })
